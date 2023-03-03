22bb510148988685d33f4e4dc1231a33173b96a9
“dariushbadakhshan” <dariush.badakhshan@gmail.com>
2023-03-03 22:21:45 +0330

    create cart

---
+++ src/component/cart/cart-item.tsx
@@ -0,0 +1,16 @@
+const CartItem = () => {
+  return (
+    <div>
+      <div>
+        <h3>name</h3>
+        <p>totalPrice</p>
+        <p>totalItemAmount</p>
+      </div>
+      <div>
+        <button>-</button>
+        <button>+</button>
+      </div>
+    </div>
+  );
+};
+export default CartItem;
